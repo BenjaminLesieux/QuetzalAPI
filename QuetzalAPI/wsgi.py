2773fd16a22afd85ef9d580e93c8137888e15423
benjaminlesieux <benjamin.lesieux@efrei.net>
2022-06-14 09:52:55 +0200

    First commit, let's vote

---
+++ QuetzalAPI/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for QuetzalAPI project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'QuetzalAPI.settings')
+
+application = get_wsgi_application()
