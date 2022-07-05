c13f6b12d97fc9a11b038d04cc816591fc266bb3
benjaminlesieux <benjamin.lesieux@efrei.net>
2022-07-05 09:26:44 +0200

    • nearest election finished

--- QuetzalAPI/settings.py
+++ QuetzalAPI/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3@8hi$kp9@j(haibiy^zh0yb_xw-^f)(zo193fc+k0(xg+kq2t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['voteatquetzal.herokuapp.com', '127.0.0.1', '*']
 
